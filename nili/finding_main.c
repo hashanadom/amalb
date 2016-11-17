b57d738fb2acb1004bfa848fc1314263e8a98e22
hashanadom <matbaranes@gmail.com>
2016-11-17 15:09:21 +0200

    Update finding_main.c

--- nili/finding_main.c
+++ nili/finding_main.c
@@ -1,4 +1,21 @@
-void find_main(char *filename){
-file *fptr=fopen(filename, 'r')
+#define n 60
+#define string_to_search "main"
 
+
+
+
+void find_main(char *filename){
+  file *fp=fopen(filename, 'r');
+  char str[n];
+  int flag=0;
+  
+  (fp == NULL)? perror("Error opening file"):0;
+  
+  while((!EOF)&&(!flag)){
+    fgets (str, n, fp);
+    flag=index(str,string_to_search)!=-1;
+  }
+  
+  flag? printf(str), while(!EOF) printf(fgets(str,n,fp)) : 0;
+  fclose(fp);
 }
