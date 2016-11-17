7df02beb7d09ce949177b67ab7f2e88cff5cf44f
hashanadom <matbaranes@gmail.com>
2016-11-18 00:33:49 +0200

    Create editfile.c

---
+++ nili/editfile.c
@@ -0,0 +1,14 @@
+void copy_2_files(char *fn1,char *fn2,char *fn3){
+file *f1=fopen(fn1,'br');
+file *f2=fopen(fn2,'ba');
+file *f3=fopen(fn3,'ba');
+
+while(!EOF){
+
+}
+
+
+fclose(f1);
+fclose(f2);
+fclose(f3);
+}
